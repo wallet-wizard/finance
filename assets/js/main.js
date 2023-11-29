3a0120d9fce28a455462c1ebd3c2eacea97ccae5
Eleni <elenimariex@gmail.com>
2023-11-29 19:37:07 +0000

    Creating a function to obtain the country's currency information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 // Function to obtain the country's currency name, code and symbol
 
+var countrySelection = prompt("Please enter your country of residence")
+
+var restCountryAPILink = "https://restcountries.com/v3.1/name/" + countrySelection
+
+fetch(restCountryAPILink).then(function(response){
+    return response.json();
+}).then(function (data){
+
+    console.log(data[0].currencies)
+
+})
+
 
 
 // Function to obtain the exchange rate, based on the country's code
