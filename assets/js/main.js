b5cbd9df13e92850d6a26c58fed662652d398b14
Eleni <elenimariex@gmail.com>
2023-11-29 20:21:23 +0000

    Creating function to obtain the country's currency code, name and symbol from API

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,41 @@ fetch(restCountryAPILink).then(function(response){
 
     console.log(data[0].currencies)
 
+    var currencyCode = Object.keys(data[0].currencies)[0]
+    var currencyName = data[0].currencies[currencyCode].name;
+    var currencySymbol = data[0].currencies[currencyCode].symbol;
+
+
+    console.log(currencyCode, currencyName, currencySymbol)
+
+    return {currencyCode, currencyName, currencySymbol}
+
 })
 
 
+// Function to obtain user's API Key for the currency API
+
+// function getAPIKey(){
+//     // var yourAPIkey = null;
+
+//     if(localStorage.getItem('api-key' === null)){
+//         yourAPIkey = prompt("Please enter your API key from currencyapi.com");
+//         localStorage.setItem('api-key', yourAPIkey)
+//     } else {
+//         yourAPIkey = localStorage.getItem('api-key')
+//     }
+
+//     return yourAPIkey
+// }
 
 // Function to obtain the exchange rate, based on the country's code
+
+
+var currencyRateLink = "https://api.currencyapi.com/v3/latest?apikey=" + yourAPIkey + "&currencies=&base_currency=" + currencyCode
+
+fetch(currencyRateLink).then(function(response){
+    return response.json();
+}).then(function (data){
+
+    console.log(data.data)
+})
