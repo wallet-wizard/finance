f96a19d3a5b1c6754f7eaa6ba51d56b458aeb989
Eleni <elenimariex@gmail.com>
2023-11-30 20:34:20 +0000

    Creating drop down menu for the welcome sign up section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 
 var countrySelection = prompt("Please enter your country of residence")
 
+var allCountriesLink = "https://restcountries.com/v3.1/all"
+
 var restCountryAPILink = "https://restcountries.com/v3.1/name/" + countrySelection
 
 
@@ -42,16 +44,36 @@ var currencyInfo = fetch(restCountryAPILink).then(function(response){
 //     return yourAPIkey
 // }
 
+
+
+
 // Function to obtain the exchange rate, based on the country's code
 
+// var currencyRateLink = "https://api.currencyapi.com/v3/latest?apikey=" + yourAPIkey + "&currencies=&base_currency=" + localStorage.getItem('currency-code')
+
+// fetch(currencyRateLink).then(function(response){
+//     return response.json();
+// }).then(function (data){
+
+//     console.log(data)
+
+// })
 
 
-var currencyRateLink = "https://api.currencyapi.com/v3/latest?apikey=" + yourAPIkey + "&currencies=&base_currency=" + localStorage.getItem('currency-code')
+// Creating a drop-down menu of all countries for the user to select from
 
-fetch(currencyRateLink).then(function(response){
-    return response.json();
-}).then(function (data){
+var countryList = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
-    console.log(data)
+function countryDropDownMenu(){
+    var countryDiv = document.getElementById("country");
 
-})
+    for(var i = 0; i < countryList.length; i++){
+        var option = document.createElement("option");
+        var textContent = document.createTextNode(countryList[i]);
+        option.appendChild(textContent)
+        countryDiv.appendChild(option);
+    }
+
+}
+
+countryDropDownMenu()
