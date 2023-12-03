1761ce15f09fcb311bbee6b80ec8d791b7de2af8
Eleni <elenimariex@gmail.com>
2023-12-03 22:34:09 +0000

    Changing name of api-main file

---
+++ assets/js/api-main.js
@@ -0,0 +1,8 @@
+// Creating section for currency exchange rates and submit button
+
+var bodyElement = document.querySelector('body');
+var sectionElement = document.createElement('section');
+sectionElement.setAttribute('id', 'currency-exchange');
+sectionElement.textContent("hello")
+
+bodyElement.appendChild(sectionElement);
