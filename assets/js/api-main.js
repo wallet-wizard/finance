d83735e526cbfb02b85a7809ede34ac037eab05e
Eleni <elenimariex@gmail.com>
2023-12-04 00:06:15 +0000

    Using allCountries API to retrieve the currency code

--- assets/js/api-main.js
+++ assets/js/api-main.js
@@ -1,8 +1,38 @@
-// Creating section for currency exchange rates and submit button
+var usersCurrency = JSON.parse(localStorage.getItem('walletWizDataSet')).billy.preferences.currency
 
-var bodyElement = document.querySelector('body');
-var sectionElement = document.createElement('section');
-sectionElement.setAttribute('id', 'currency-exchange');
-sectionElement.textContent("hello")
+console.log(usersCurrency)
 
-bodyElement.appendChild(sectionElement);
+
+// Need to find the user's prefered currency and use the currency code for currency API
+
+var allCountries = "https://restcountries.com/v3.1/all"
+
+fetch(allCountries).then(function(response){
+
+    return response.json();
+}).then(function (data){
+
+    for(var d = 0; d < data.length; d++){
+        data[d].currencies ? data[d].currencies[Object.keys(data[d].currencies)[0]].name : ''
+        if (data[d].currencies[Object.keys(data[d].currencies)[0]].name === usersCurrency) {
+            var currencyCode = Object.keys(data[d].currencies)[0]
+        }
+        }
+
+        return currencyCode
+    })
+        
+    
+
+
+// Function to obtain the exchange rate, based on the country's code
+
+// var currencyRateLink = "https://api.currencyapi.com/v3/latest?apikey=" + yourAPIkey + "&currencies=&base_currency=" + usersCountry
+
+// fetch(currencyRateLink).then(function(response){
+//     return response.json();
+// }).then(function (data){
+
+//     console.log(data)
+
+// })
